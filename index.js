92be05a232a205af568aac0ea7cf7c58e2b906a6
Zoulou <zouloublog@outlook.com>
2023-09-24 16:13:36 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//Checking to see if my javascript works
+console.log("Your script is working");
+
+//current page href
+var url = window.location.href;
+var main = document.getElementById("home__link");
+var shows = document.getElementById("shows__link");
+
+// Function to check if a string ends with a given string (This code isn't mine, I was struggling and got an idea from stackoverflow)
+function endsWith(str, suffix) {
+  return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+//print href to console
+console.log(url);
+
+//Here is checking for the url ending, styles will be applied.
+if (endsWith(url, "index.html")) {
+  main.style.color = "#AFAFAF";
+  main.style.borderBottom = "3px solid #AFAFAF";
+} else if (endsWith(url, "shows.html")) {
+  shows.style.color = "#AFAFAF";
+  SHOWS.style.borderBottom = "3px solid #AFAFAF";
+}
