5224e0fbca127eaf212ceb123e95d0a00b32209a
KingChoices <81588553+KingChoices@users.noreply.github.com>
2023-11-05 11:44:06 -0500

    sprint-3 - fixed navbar - comments/shows now come from api call - fixed few design errors - fixed avatar resizing

--- comments.js
+++ comments.js
@@ -1,164 +1,25 @@
-//Comment array and objects
-// var commentsArr = [
-//   {
-//     name: "Connor Walton",
-//     timestamp: "15/14/2023",
-//     commentText:
-//       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-//   },
-//   {
-//     name: "Emilie Beach",
-//     timestamp: "15/14/2023",
-//     commentText:
-//       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-//   },
-//   {
-//     name: "Miles Acosta",
-//     timestamp: "15/14/2023",
-//     commentText:
-//       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-//   },
-// ];
-//Log to console to make sure array is working
-// console.log(commentsArr);
-
-//Getting my container where I want to display the comments
-
-//This loop will loop through the array and display every comment it finds.
-// function renderComments(newComment) {
-//   for (var i = 0; i < commentsArr.length; i++) {
-//     var comment = commentsArr[i];
-//     const commentDiv = document.createElement("div");
-//     const innerDivOne = document.createElement("div");
-//     const img = document.createElement("img");
-//     const hrTop = document.createElement("hr");
-//     const hrBottom = document.createElement("hr");
-//     innerDivOne.appendChild(img);
-
-//     const innerDivTwo = document.createElement("div");
-//     const innerDivThree = document.createElement("div");
-//     const innerDivFour = document.createElement("div");
-//     const innerDivFive = document.createElement("div");
-//     const innerDivSix = document.createElement("div");
-
-//     const heading = document.createElement("h4");
-//     heading.textContent = comment.name;
-//     const paragraph1 = document.createElement("p");
-//     paragraph1.textContent = comment.timestamp;
-
-//     innerDivFour.appendChild(heading);
-//     innerDivFive.appendChild(paragraph1);
-
-//     innerDivTwo.appendChild(innerDivThree);
-//     innerDivTwo.appendChild(innerDivSix);
-//     innerDivThree.appendChild(innerDivFour);
-//     innerDivThree.appendChild(innerDivFive);
-
-//     const paragraph2 = document.createElement("p");
-//     paragraph2.textContent = comment.commentText;
-
-//     innerDivSix.appendChild(paragraph2);
-
-//     commentDiv.appendChild(innerDivOne);
-//     commentDiv.appendChild(innerDivTwo);
-
-//     commentDiv.classList.add("post__container");
-//     innerDivOne.classList.add("post__container--imgdiv");
-//     innerDivTwo.classList.add("post__container--textdiv");
-//     innerDivThree.classList.add("post__container--infodiv");
-//     innerDivFour.classList.add("post__container--namediv");
-//     innerDivFive.classList.add("post__container--datediv");
-//     innerDivSix.classList.add("post__container--commentdiv");
-//     hrTop.classList.add("comments__hr");
-
-//     commentsContainer.appendChild(commentDiv);
-//     commentsContainer.appendChild(hrTop);
-//   }
-// }
-
-//Form functionality
-// var formId = document.getElementById("comment__form");
-
-// var nameId = document.getElementById("name");
-// var commentId = document.getElementById("comment");
-// var submitId = document.getElementById("submit");
-
-// formId.addEventListener("submit", formSub);
-
-// const date = new Date();
-// var day = date.getDate();
-// var month = date.getMonth() + 1;
-// var year = date.getFullYear();
-// const displayDate = day + "/" + month + "/" + year;
-
-// console.log(displayDate);
-// console.log(nameId.value + commentId.value);
-
-//Form Submission
-// function formSub(event) {
-//   event.preventDefault();
-//   console.log("Form submitted!");
-//   const name = nameId.value;
-//   const comment = commentId.value;
-//   console.log("Name:", name);
-//   console.log("Comment:", comment);
-//   const newComment = {
-//     name: name,
-//     timestamp: displayDate,
-//     commentText: comment,
-//   };
-
-//   console.log("New Comment:", newComment);
-//   commentsArr.push(newComment);
-
-//   nameId.value = "";
-//   commentId.value = "";
-
-//   //On submission this will clear the comments array and res-display all comments with the new one.
-//   commentsContainer.innerHTML = "";
-//   renderComments(newComment);
-// }
-
-//renderComments();
-
-function registerAndStoreKey() { //make key static 
-  axios
-    .get("https://project-1-api.herokuapp.com/register")
-    .then((response) => {
-      const key = response.data.api_key;
-
-      localStorage.setItem("apiKey", key);
-
-      console.log("api key is working.", key);
-    })
-    .catch((error) => {
-      console.log("no api key");
-    });
-}
-
-let api_data;
-const dataArr = [];
+const commentsContainer = document.getElementById("comments");
 
 function apiRequest(endpoint) {
-  const key = localStorage.getItem("apiKey");
+  const key = "dfbea7b2-c953-4935-8032-7e2755fd6d8f";  //store key from url/register
 
   if (!key) {
     console.error("key not found. Please register first.");
   }
 
   axios
-    .get(`https://project-1-api.herokuapp.com/${endpoint}?api_key=${key}`)
+    .get(`https://project-1-api.herokuapp.com/${endpoint}?api_key=${key}`) //fetch the data from api
     .then((response) => {
       console.log("API request successful:", response.data);
       function renderApiComments(newApiComment) {
-        let reverseArr = response.data.reverse();
-        for (var i = 0; i < reverseArr.length; i++) {
+        let reverseArr = response.data.reverse(); //reverse the array to display newly added items first
+        for (var i = 0; i < reverseArr.length; i++) { //loop through the array
           if (
             reverseArr[i].name != null &&
             reverseArr[i].comment != null
-          ) {
+          ) { // checks to make sure the comments in api have a valid name and comment before displaying
             var info = reverseArr[i];
-            const commentDiv = document.createElement("div");
+            const commentDiv = document.createElement("div"); 
             const innerDivOne = document.createElement("div");
             const img = document.createElement("img");
             const hrTop = document.createElement("hr");
@@ -206,7 +67,7 @@ function apiRequest(endpoint) {
         }
       }
 
-      renderApiComments();
+      renderApiComments(); // render all comments on page load
 
       var formId = document.getElementById("comment__form");
 
@@ -214,7 +75,7 @@ function apiRequest(endpoint) {
       var commentId = document.getElementById("comment");
       var submitId = document.getElementById("submit");
 
-      formId.addEventListener("submit", formSub);
+      formId.addEventListener("submit", formSub); // form submission handler
 
       const date = new Date();
       var day = date.getDate();
@@ -223,7 +84,6 @@ function apiRequest(endpoint) {
       const displayDate = day + "/" + month + "/" + year;
 
       console.log(displayDate);
-      console.log(nameId.value + commentId.value);
 
       function formSub(event) {
         event.preventDefault();
@@ -241,7 +101,7 @@ function apiRequest(endpoint) {
           comment: comment,
         };
 
-        axios.post(`https://project-1-api.herokuapp.com/${endpoint}?api_key=${key}`, 
+        axios.post(`https://project-1-api.herokuapp.com/${endpoint}?api_key=${key}`, //posting to the api to store comments
           newComment
         )
           .then((commentCreated) => {
@@ -267,77 +127,4 @@ function apiRequest(endpoint) {
     });
 }
 
-registerAndStoreKey();
 apiRequest("comments");
-// function newComment(name, comment) {
-//   const headers = {
-//     "Content-Type": "application/json",
-//   };
-
-//   const date = new Date();
-//   var day = date.getDate();
-//   var month = date.getMonth() + 1;
-//   var year = date.getFullYear();
-//   const displayDate = day + "/" + month + "/" + year;
-
-//   const newApiComment = {
-//     name: nameId.value,
-//     timestamp: displayDate,
-//     comment: commentId.value,
-//   };
-// }
-
-const commentsContainer = document.getElementById("comments");
-
-// function renderApiComments(newApiComment) {
-//   for (var i = 0; i < dataArr.length; i++) {
-//     console.log("Loop is working");
-//     var info = dataArr[i];
-//     const commentDiv = document.createElement("div");
-//     const innerDivOne = document.createElement("div");
-//     const img = document.createElement("img");
-//     const hrTop = document.createElement("hr");
-//     innerDivOne.appendChild(img);
-
-//     const innerDivTwo = document.createElement("div");
-//     const innerDivThree = document.createElement("div");
-//     const innerDivFour = document.createElement("div");
-//     const innerDivFive = document.createElement("div");
-//     const innerDivSix = document.createElement("div");
-
-//     const heading = document.createElement("h4");
-//     heading.textContent = info.name;
-//     const paragraph1 = document.createElement("p");
-//     paragraph1.textContent = info.timestamp;
-
-//     innerDivFour.appendChild(heading);
-//     innerDivFive.appendChild(paragraph1);
-
-//     innerDivTwo.appendChild(innerDivThree);
-//     innerDivTwo.appendChild(innerDivSix);
-//     innerDivThree.appendChild(innerDivFour);
-//     innerDivThree.appendChild(innerDivFive);
-
-//     const paragraph2 = document.createElement("p");
-//     paragraph2.textContent = info.comment;
-
-//     innerDivSix.appendChild(paragraph2);
-
-//     commentDiv.appendChild(innerDivOne);
-//     commentDiv.appendChild(innerDivTwo);
-
-//     commentDiv.classList.add("post__container");
-//     innerDivOne.classList.add("post__container--imgdiv");
-//     innerDivTwo.classList.add("post__container--textdiv");
-//     innerDivThree.classList.add("post__container--infodiv");
-//     innerDivFour.classList.add("post__container--namediv");
-//     innerDivFive.classList.add("post__container--datediv");
-//     innerDivSix.classList.add("post__container--commentdiv");
-//     hrTop.classList.add("comments__hr");
-
-//     commentsContainer.appendChild(commentDiv);
-//     commentsContainer.appendChild(hrTop);
-//   }
-// }
-
-// renderApiComments();
